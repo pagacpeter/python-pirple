bd704d3554f3257d334436d6f4ba721b7c311d5f
Peter Pagac <pagacpeter@yahoo.com>
2021-03-14 00:48:06 +0100

    pirple homework#1

---
+++ main.py
@@ -0,0 +1,44 @@
+"""
+My favourite song - Bob Dylan - The Times They are a-Changin
+
+"""
+#basic info
+SongName = "The Times They are a-Changin"
+Artist = "Bob Dylan"
+Genre = "Folk"
+Length = 3.15
+
+#recording info
+
+Recorded = "October 24, 1963"
+Studio = "Columbia Recording"
+StudioLocation = "New York City"
+RecordLabel = "Columbia"
+
+#release info
+OnAlbum = "The Times They are a-Changin"
+AlbumReleaseDate = "January 13, 1964"
+SingleRelease = "yes"
+SingleReleaseDate = "March 8, 1965"
+SingleBSide = "Honey, Just Allow Me One More Chance"
+Producer = "Tom Wilson"
+
+# Rolling Stone's magazine 2004 chart of 500 Greatest Songs of All Time
+Top500SongsOfAllTime = 59
+
+print(SongName)
+print(Artist)
+print(Genre)
+print(Length)
+print(Recorded)
+print(Studio)
+print(StudioLocation)
+print(RecordLabel)
+print(OnAlbum)
+print(AlbumReleaseDate)
+print(SingleRelease)
+print(SingleReleaseDate)
+print(SingleBSide)
+print(Producer)
+print(Top500SongOfAllTime)
+
